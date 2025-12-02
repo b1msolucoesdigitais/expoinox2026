aab447d3b6783d860cf8f9791def622757d9b347
B1M Soluções Digitais <b1msolucoesdigitais@gmail.com>
2025-12-02 14:26:42 -0300

    fix rss

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -4,7 +4,7 @@ import { XMLParser } from 'fast-xml-parser';
 export async function GET() {
   try {
     const response = await fetch('https://maisvip.com.br/categoria/expoinox/feed/gn', {
-      next: { revalidate: 3600 },
+      cache: 'no-store',
       headers: {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
       },
